3ea5adf5e952a238609571d7ed34d9cd0d916829
Oleksandr Garmatin <invizible.soft@gmail.com>
2019-10-17 15:41:22 +0300

    Fix hotspot on non-standart tether interface name

--- tordnscrypt/src/main/java/pan/alexander/tordnscrypt/utils/Tethering.java
+++ tordnscrypt/src/main/java/pan/alexander/tordnscrypt/utils/Tethering.java
@@ -23,8 +23,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 import pan.alexander.tordnscrypt.settings.PathVars;
+
+import static pan.alexander.tordnscrypt.utils.RootExecService.LOG_TAG;
 
 public class Tethering {
     private Context context;
@@ -62,6 +70,9 @@ public class Tethering {
         blockHotspotHttp = shPref.getBoolean("pref_common_block_http",false);
         boolean apIsOn = new PrefManager(context).getBoolPref("APisON");
 
+        String hotspotInterfaceName = getHotspotInterfaceName();
+        Log.i(LOG_TAG, "HOTSPOT interface name " + hotspotInterfaceName);
+
         String torSitesBypassPreroutingTCP = "";
         String torSitesBypassForwardTCP = "";
         String torSitesBypassPreroutingUDP = "";
@@ -81,8 +92,8 @@ public class Tethering {
         if (blockHotspotHttp) {
             blockHttpRuleForwardTCP = iptablesPath + "iptables -A tordnscrypt_forward -p tcp --dport 80 -j REJECT";
             blockHttpRuleForwardUDP = iptablesPath + "iptables -A tordnscrypt_forward -p udp --dport 80 -j REJECT";
-            blockHttpRulePreroutingTCP = iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp ! -d 192.168.43.0/24 --dport 80 -j RETURN";
-            blockHttpRulePreroutingUDP = iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p udp ! -d 192.168.43.0/24 --dport 80 -j RETURN";
+            blockHttpRulePreroutingTCP = iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp ! -d 192.168.43.0/24 --dport 80 -j RETURN";
+            blockHttpRulePreroutingUDP = iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p udp ! -d 192.168.43.0/24 --dport 80 -j RETURN";
         }
 
         if (!torTethering && !itpdTethering) {
@@ -127,14 +138,14 @@ public class Tethering {
                         iptablesPath+ "iptables -I tordnscrypt -p udp -m udp --sport 67 -j ACCEPT",
                         iptablesPath+ "iptables -I tordnscrypt -p udp -m udp --sport 68 -j ACCEPT",
                         busyboxPath+ "sleep 1",
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p udp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p udp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
                         iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -p tcp -d "+torVirtAdrNet+" -j REDIRECT --to-ports "+torTransPort,
                         blockHttpRulePreroutingTCP,
                         blockHttpRulePreroutingUDP,
                         torSitesBypassPreroutingTCP,
                         torSitesBypassPreroutingUDP,
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp ! -d 192.168.43.0/24 -j REDIRECT --to-ports "+ torTransPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp ! -d 192.168.43.0/24 -j REDIRECT --to-ports "+ torTransPort,
                         iptablesPath + "iptables -A tordnscrypt_forward -p udp --dport 53 -j RETURN",
                         iptablesPath + "iptables -A tordnscrypt_forward -p tcp --dport 53 -j RETURN",
                         blockHttpRuleForwardTCP,
@@ -177,13 +188,13 @@ public class Tethering {
                         iptablesPath+ "iptables -I tordnscrypt -p udp -m udp --sport 67 -j ACCEPT",
                         iptablesPath+ "iptables -I tordnscrypt -p udp -m udp --sport 68 -j ACCEPT",
                         busyboxPath+ "sleep 1",
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p udp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p udp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
                         iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -p tcp -d "+torVirtAdrNet+" -j REDIRECT --to-ports "+torTransPort,
                         blockHttpRulePreroutingTCP,
                         blockHttpRulePreroutingUDP,
                         busyboxPath+ "sleep 1",
-                        busyboxPath + "cat "+ appDataDir +"/app_data/tor/unlock_tether | while read var1; do "+ iptablesPath +"iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp -d $var1 -j REDIRECT --to-port "+ torTransPort +"; done",
+                        busyboxPath + "cat "+ appDataDir +"/app_data/tor/unlock_tether | while read var1; do "+ iptablesPath +"iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp -d $var1 -j REDIRECT --to-port "+ torTransPort +"; done",
                         iptablesPath + "iptables -A tordnscrypt_forward -p tcp --dport 53 -j RETURN",
                         iptablesPath + "iptables -A tordnscrypt_forward -p udp --dport 53 -j RETURN",
                         blockHttpRuleForwardTCP,
@@ -222,8 +233,8 @@ public class Tethering {
                         iptablesPath+ "iptables -I tordnscrypt -p udp -m udp --sport 67 -j ACCEPT",
                         iptablesPath+ "iptables -I tordnscrypt -p udp -m udp --sport 68 -j ACCEPT",
                         busyboxPath+ "sleep 1",
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p udp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p udp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
                         iptablesPath + "iptables -A tordnscrypt_forward -p tcp --dport 53 -j RETURN",
                         iptablesPath + "iptables -A tordnscrypt_forward -p udp --dport 53 -j RETURN",
                         blockHttpRuleForwardTCP,
@@ -267,7 +278,7 @@ public class Tethering {
                         blockHttpRulePreroutingUDP,
                         torSitesBypassPreroutingTCP,
                         torSitesBypassPreroutingUDP,
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp ! -d 192.168.43.0/24 -j REDIRECT --to-ports "+ torTransPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp ! -d 192.168.43.0/24 -j REDIRECT --to-ports "+ torTransPort,
                         iptablesPath + "iptables -A tordnscrypt_forward -p tcp --dport 53 -j RETURN",
                         iptablesPath + "iptables -A tordnscrypt_forward -p udp --dport 53 -j RETURN",
                         blockHttpRuleForwardTCP,
@@ -314,7 +325,7 @@ public class Tethering {
                         blockHttpRulePreroutingTCP,
                         blockHttpRulePreroutingUDP,
                         busyboxPath+ "sleep 1",
-                        busyboxPath + "cat "+ appDataDir +"/app_data/tor/unlock_tether | while read var1; do "+ iptablesPath +"iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp -d $var1 -j REDIRECT --to-port "+ torTransPort +"; done",
+                        busyboxPath + "cat "+ appDataDir +"/app_data/tor/unlock_tether | while read var1; do "+ iptablesPath +"iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp -d $var1 -j REDIRECT --to-port "+ torTransPort +"; done",
                         iptablesPath + "iptables -A tordnscrypt_forward -p tcp --dport 53 -j RETURN",
                         iptablesPath + "iptables -A tordnscrypt_forward -p udp --dport 53 -j RETURN",
                         blockHttpRuleForwardTCP,
@@ -355,14 +366,14 @@ public class Tethering {
                         iptablesPath+ "iptables -I tordnscrypt -d 127.0.0.1/32 -p udp -m udp --dport "+torDNSPort+" -j ACCEPT",
                         iptablesPath+ "iptables -I tordnscrypt -d 127.0.0.1/32 -p tcp -m tcp --dport "+torDNSPort+" -j ACCEPT",
                         busyboxPath+ "sleep 1",
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p udp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p udp -d 10.191.0.1 -j REDIRECT --to-ports "+ itpdHttpProxyPort,
                         iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -p tcp -d "+torVirtAdrNet+" -j REDIRECT --to-ports "+torTransPort,
                         blockHttpRulePreroutingTCP,
                         blockHttpRulePreroutingUDP,
                         torSitesBypassPreroutingTCP,
                         torSitesBypassPreroutingUDP,
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp ! -d 192.168.43.0/24 -j REDIRECT --to-ports "+ torTransPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp ! -d 192.168.43.0/24 -j REDIRECT --to-ports "+ torTransPort,
                         iptablesPath + "iptables -A tordnscrypt_forward -p tcp --dport 53 -j RETURN",
                         iptablesPath + "iptables -A tordnscrypt_forward -p udp --dport 53 -j RETURN",
                         blockHttpRuleForwardTCP,
@@ -410,7 +421,7 @@ public class Tethering {
                         blockHttpRulePreroutingUDP,
                         torSitesBypassPreroutingTCP,
                         torSitesBypassPreroutingUDP,
-                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i wlan0 -p tcp ! -d 192.168.43.0/24 -j REDIRECT --to-ports "+ torTransPort,
+                        iptablesPath + "iptables -t nat -A tordnscrypt_prerouting -i " + hotspotInterfaceName + " -p tcp ! -d 192.168.43.0/24 -j REDIRECT --to-ports "+ torTransPort,
                         iptablesPath + "iptables -A tordnscrypt_forward -p tcp --dport 53 -j RETURN",
                         iptablesPath + "iptables -A tordnscrypt_forward -p udp --dport 53 -j RETURN",
                         blockHttpRuleForwardTCP,
@@ -503,5 +514,42 @@ public class Tethering {
             };
             handler.postDelayed(runTethering,1000);
         }
+    }
+
+    private String getHotspotInterfaceName() {
+        String name = "wlan0";
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+                 en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                if (intf.isLoopback()) {
+                    continue;
+                }
+                if (intf.isVirtual()) {
+                    continue;
+                }
+                if (!intf.isUp()) {
+                    continue;
+                }
+                if (intf.isPointToPoint()) {
+                    continue;
+                }
+                if (intf.getHardwareAddress() == null) {
+                    continue;
+                }
+
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses();
+                     enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (inetAddress.getHostAddress().contains("192.168.43.")) {
+                        name = intf.getName();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Log.e(LOG_TAG, e.getMessage() + " " + e.getCause());
+        }
+
+        return name;
     }
 }
