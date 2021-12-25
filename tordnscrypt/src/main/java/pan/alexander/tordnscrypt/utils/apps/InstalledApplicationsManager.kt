d606dc57d5c8e8d27260d4d43ad01e11801b7167
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-12-25 21:21:03 +0200

    Improve installed apps handling

--- tordnscrypt/src/main/java/pan/alexander/tordnscrypt/utils/apps/InstalledApplicationsManager.kt
+++ tordnscrypt/src/main/java/pan/alexander/tordnscrypt/utils/apps/InstalledApplicationsManager.kt
@@ -28,42 +28,40 @@ import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Process
 import android.os.UserManager
-import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
+import pan.alexander.tordnscrypt.App
 import pan.alexander.tordnscrypt.settings.tor_apps.ApplicationData
-import pan.alexander.tordnscrypt.utils.root.RootExecService.LOG_TAG
+import pan.alexander.tordnscrypt.utils.logger.Logger.loge
+import pan.alexander.tordnscrypt.utils.logger.Logger.logi
+import pan.alexander.tordnscrypt.utils.logger.Logger.logw
+import pan.alexander.tordnscrypt.utils.preferences.PreferenceKeys.FIREWALL_SHOWS_ALL_APPS
+import pan.alexander.tordnscrypt.utils.preferences.PreferenceKeys.MULTI_USER_SUPPORT
 import java.util.concurrent.locks.ReentrantLock
 import java.util.regex.Pattern
 
+private const val ON_APP_ADDED_REFRESH_PERIOD_MSEC = 250
+private val pattern: Pattern = Pattern.compile("UserHandle\\{(\\d+)\\}")
+private val reentrantLock = ReentrantLock()
 
-class InstalledApplicationsManager(private val context: Context, private val activeApps: Set<String>) {
+class InstalledApplicationsManager private constructor(
+    private var onAppAddListener: OnAppAddListener?,
+    private val activeApps: Set<String>,
+    private val showSpecialApps: Boolean,
+    private var showAllApps: Boolean?
+) {
 
-    private companion object{
-        private val pattern: Pattern = Pattern.compile("UserHandle\\{(.*)\\}")
-        private val reentrantLock = ReentrantLock()
-    }
-
-    private var onAppAddListener: OnAppAddListener? = null
-
-    fun setOnAppAddListener(onAppAddListener: OnAppAddListener?) {
-        this.onAppAddListener = onAppAddListener
-    }
-
-    interface OnAppAddListener {
-        fun onAppAdded(application: ApplicationData)
-    }
-
+    private val context = App.instance.applicationContext
     private val ownUID = Process.myUid()
-    private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-    private val multiUserSupport = sharedPreferences.getBoolean("pref_common_multi_user", false)
-    private var showSpecials = false
+    private var sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    private var multiUserSupport = sharedPreferences.getBoolean(MULTI_USER_SUPPORT, false)
     private var savedTime = 0L
-    private val onAppAddedRefreshPeriod = 250
 
-    fun getInstalledApps(showSpecials: Boolean = false): List<ApplicationData> {
+    init {
+        showAllApps = showAllApps ?: sharedPreferences.getBoolean(FIREWALL_SHOWS_ALL_APPS, false)
+    }
 
-        this.showSpecials = showSpecials
+    fun getInstalledApps(): List<ApplicationData> {
 
         try {
 
@@ -88,7 +86,7 @@ class InstalledApplicationsManager(private val context: Context, private val act
                     }
                 }
 
-                Log.i(LOG_TAG, "Devise Users: ${uids.joinToString()}")
+                logi("Devise Users: ${uids.joinToString()}")
             }
 
             var pkgManagerFlags = PackageManager.GET_META_DATA
@@ -110,42 +108,43 @@ class InstalledApplicationsManager(private val context: Context, private val act
             installedApps.forEach { applicationInfo ->
                 application = userAppsMap[applicationInfo.uid]
 
-                val name = packageManager.getApplicationLabel(applicationInfo)?.toString() ?: "Undefined"
+                val name =
+                    packageManager.getApplicationLabel(applicationInfo)?.toString() ?: "Undefined"
                 val icon = packageManager.getApplicationIcon(applicationInfo)
 
                 if (application == null) {
-                    val system = (applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0
-                    var useInternet = false
                     val uid = applicationInfo.uid
 
-                    try {
-                        val pInfo: PackageInfo = packageManager.getPackageInfo(applicationInfo.packageName, PackageManager.GET_PERMISSIONS)
-                        if (pInfo.requestedPermissions != null) {
-                            for (permInfo in pInfo.requestedPermissions) {
-                                if (permInfo == Manifest.permission.INTERNET) {
-                                    useInternet = true
-                                    break
-                                }
-                            }
-                        }
-                    } catch (e: Exception) {
-                        useInternet = true
-                        Log.w(LOG_TAG, "InstalledApplications getApp exception  ${e.message}\n${e.cause}")
+                    if (uid == ownUID) {
+                        return@forEach
                     }
 
-                    if (!useInternet && !system || uid == ownUID) {
-                        return@forEach
+                    val system = (applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0
+
+                    if (!system && showAllApps == false) {
+                        val useInternet = isAppUseInternet(packageManager, applicationInfo)
+
+                        if (!useInternet) {
+                            return@forEach
+                        }
                     }
 
                     val packageName = applicationInfo.packageName
 
-                    application = ApplicationData(name, packageName, uid, icon, system, activeApps.contains(uid.toString()))
+                    application = ApplicationData(
+                        name,
+                        packageName,
+                        uid,
+                        icon,
+                        system,
+                        activeApps.contains(uid.toString())
+                    )
 
                     if ((applicationInfo.flags and ApplicationInfo.FLAG_INSTALLED) != 0) {
                         application?.let {
                             userAppsMap[uid] = it
                             val time = System.currentTimeMillis()
-                            if (time - savedTime > onAppAddedRefreshPeriod) {
+                            if (time - savedTime > ON_APP_ADDED_REFRESH_PERIOD_MSEC) {
                                 onAppAddListener?.onAppAdded(it)
                                 savedTime = time
                             }
@@ -156,7 +155,14 @@ class InstalledApplicationsManager(private val context: Context, private val act
                 }
 
                 if (uids.size > 1 || uids.getOrElse(0) { 0 } != 0) {
-                    val tempMultiUserAppsMap: Map<Int, ApplicationData> = checkPartOfMultiUser(applicationInfo, name, icon, uids, packageManager, multiUserAppsMap)
+                    val tempMultiUserAppsMap: Map<Int, ApplicationData> = checkPartOfMultiUser(
+                        applicationInfo,
+                        name,
+                        icon,
+                        uids,
+                        packageManager,
+                        multiUserAppsMap
+                    )
                     tempMultiUserAppsMap.forEach { (uid, applicationData) ->
                         if (multiUserAppsMap.containsKey(uid)) {
                             multiUserAppsMap[uid]?.addAllNames(applicationData.names)
@@ -185,8 +191,9 @@ class InstalledApplicationsManager(private val context: Context, private val act
 
             return applications.sorted()
         } catch (e: Exception) {
-            Log.e(LOG_TAG, "InstalledApplications getInstalledApps exception ${e.message}\n${e.cause}\n${e.stackTrace}")
+            loge("InstalledApplications getInstalledApps")
         } finally {
+            onAppAddListener = null
             if (reentrantLock.isLocked && reentrantLock.isHeldByCurrentThread) {
                 reentrantLock.unlock()
             }
@@ -194,9 +201,38 @@ class InstalledApplicationsManager(private val context: Context, private val act
         return emptyList()
     }
 
-    private fun checkPartOfMultiUser(applicationInfo: ApplicationInfo, name: String, icon: Drawable, uids: List<Int>,
-                                     packageManager: PackageManager,
-                                     multiUserAppsMap: Map<Int, ApplicationData>): Map<Int, ApplicationData> {
+    private fun isAppUseInternet(
+        packageManager: PackageManager,
+        applicationInfo: ApplicationInfo
+    ): Boolean {
+        var useInternet = false
+        try {
+            val pInfo: PackageInfo = packageManager.getPackageInfo(
+                applicationInfo.packageName,
+                PackageManager.GET_PERMISSIONS
+            )
+            if (pInfo.requestedPermissions != null) {
+                for (permInfo in pInfo.requestedPermissions) {
+                    if (permInfo == Manifest.permission.INTERNET) {
+                        useInternet = true
+                        break
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            useInternet = true
+            logw("InstalledApplications isAppUseInternet", e)
+        }
+        return useInternet
+    }
+
+    private fun checkPartOfMultiUser(
+        applicationInfo: ApplicationInfo,
+        name: String,
+        icon: Drawable, uids: List<Int>,
+        packageManager: PackageManager,
+        multiUserAppsMap: Map<Int, ApplicationData>
+    ): Map<Int, ApplicationData> {
 
         val tempMultiUserAppsMap = hashMapOf<Int, ApplicationData>()
 
@@ -210,21 +246,24 @@ class InstalledApplicationsManager(private val context: Context, private val act
                     packages?.let {
                         val system = (applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0
                         val packageName = it.joinToString()
-                        val application = ApplicationData("$name(M)", packageName, applicationUID,
-                                icon, system, activeApps.contains(applicationUID.toString()))
+                        val application = ApplicationData(
+                            "$name(M)", packageName, applicationUID,
+                            icon, system, activeApps.contains(applicationUID.toString())
+                        )
 
                         tempMultiUserAppsMap[applicationUID] = application
 
                         val time = System.currentTimeMillis()
 
                         if (!multiUserAppsMap.containsKey(applicationUID)
-                                && time - savedTime > onAppAddedRefreshPeriod) {
+                            && time - savedTime > ON_APP_ADDED_REFRESH_PERIOD_MSEC
+                        ) {
                             onAppAddListener?.onAppAdded(application)
                             savedTime = time
                         }
                     }
-                } catch (e: java.lang.Exception) {
-                    Log.e(LOG_TAG, "checkPartOfMultiUser exception ${e.message} ${e.cause}")
+                } catch (e: Exception) {
+                    loge("InstalledApplications checkPartOfMultiUser")
                 }
             }
         }
@@ -245,27 +284,108 @@ class InstalledApplicationsManager(private val context: Context, private val act
         val shell = getUidForName("shell", 2000 + userId * 100_000)
         val clat = getUidForName("clat", 1029 + userId * 100_000)
         val specialDataApps = arrayListOf(
-                ApplicationData("Kernel", "UID -1", -1, defaultIcon, true, activeApps.contains("-1")),
-                ApplicationData("Root", "root", 0, defaultIcon, true, activeApps.contains("0")),
-                ApplicationData("Android Debug Bridge", "adb", adb, defaultIcon, true, activeApps.contains(adb.toString())),
-                ApplicationData("Media server", "media", media, defaultIcon, true, activeApps.contains(media.toString())),
-                ApplicationData("VPN", "vpn", vpn, defaultIcon, true, activeApps.contains(vpn.toString())),
-                ApplicationData("Digital Rights Management", "drm", drm, defaultIcon, true, activeApps.contains(drm.toString())),
-                ApplicationData("Multicast DNS", "mDNS", mdns, defaultIcon, true, activeApps.contains(mdns.toString())),
-                ApplicationData("GPS", "gps", gps, defaultIcon, true, activeApps.contains(gps.toString())),
-                ApplicationData("DNS", "dns", dns, defaultIcon, true, activeApps.contains(dns.toString())),
-                ApplicationData("Linux shell", "shell", shell, defaultIcon, true, activeApps.contains(shell.toString()))
+            ApplicationData("Kernel", "UID -1", -1, defaultIcon, true, activeApps.contains("-1")),
+            ApplicationData("Root", "root", 0, defaultIcon, true, activeApps.contains("0")),
+            ApplicationData(
+                "Android Debug Bridge",
+                "adb",
+                adb,
+                defaultIcon,
+                true,
+                activeApps.contains(adb.toString())
+            ),
+            ApplicationData(
+                "Media server",
+                "media",
+                media,
+                defaultIcon,
+                true,
+                activeApps.contains(media.toString())
+            ),
+            ApplicationData(
+                "VPN",
+                "vpn",
+                vpn,
+                defaultIcon,
+                true,
+                activeApps.contains(vpn.toString())
+            ),
+            ApplicationData(
+                "Digital Rights Management",
+                "drm",
+                drm,
+                defaultIcon,
+                true,
+                activeApps.contains(drm.toString())
+            ),
+            ApplicationData(
+                "Multicast DNS",
+                "mDNS",
+                mdns,
+                defaultIcon,
+                true,
+                activeApps.contains(mdns.toString())
+            ),
+            ApplicationData(
+                "GPS",
+                "gps",
+                gps,
+                defaultIcon,
+                true,
+                activeApps.contains(gps.toString())
+            ),
+            ApplicationData(
+                "DNS",
+                "dns",
+                dns,
+                defaultIcon,
+                true,
+                activeApps.contains(dns.toString())
+            ),
+            ApplicationData(
+                "Linux shell",
+                "shell",
+                shell,
+                defaultIcon,
+                true,
+                activeApps.contains(shell.toString())
+            )
         )
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            specialDataApps.add(ApplicationData("Clat", "clat", clat, defaultIcon, true, activeApps.contains(clat.toString())))
+            specialDataApps.add(
+                ApplicationData(
+                    "Clat",
+                    "clat",
+                    clat,
+                    defaultIcon,
+                    true,
+                    activeApps.contains(clat.toString())
+                )
+            )
         }
 
-        if (showSpecials) {
-            specialDataApps.add(ApplicationData("Internet time servers", "ntp", ApplicationData.SPECIAL_UID_NTP, defaultIcon,
-                    true, activeApps.contains(ApplicationData.SPECIAL_UID_NTP.toString())))
-            specialDataApps.add(ApplicationData("A-GPS", "agps", ApplicationData.SPECIAL_UID_AGPS, defaultIcon,
-                    true, activeApps.contains(ApplicationData.SPECIAL_UID_AGPS.toString())))
+        if (showSpecialApps) {
+            specialDataApps.add(
+                ApplicationData(
+                    "Internet time servers",
+                    "ntp",
+                    ApplicationData.SPECIAL_UID_NTP,
+                    defaultIcon,
+                    true,
+                    activeApps.contains(ApplicationData.SPECIAL_UID_NTP.toString())
+                )
+            )
+            specialDataApps.add(
+                ApplicationData(
+                    "A-GPS",
+                    "agps",
+                    ApplicationData.SPECIAL_UID_AGPS,
+                    defaultIcon,
+                    true,
+                    activeApps.contains(ApplicationData.SPECIAL_UID_AGPS.toString())
+                )
+            )
         }
 
         return specialDataApps
@@ -278,11 +398,51 @@ class InstalledApplicationsManager(private val context: Context, private val act
             if (result > 0) {
                 uid = result
             } else {
-                Log.w(LOG_TAG, "No uid for $name, using default value $defaultValue")
+                logw("No uid for $name, using default value $defaultValue")
             }
         } catch (e: Exception) {
-            Log.w(LOG_TAG, "No uid for $name, using default value $defaultValue")
+            logw("No uid for $name, using default value $defaultValue")
         }
         return uid
+    }
+
+    interface OnAppAddListener {
+        fun onAppAdded(application: ApplicationData)
+    }
+
+    class Builder {
+
+        private var onAppAddListener: OnAppAddListener? = null
+        private var activeApps = setOf<String>()
+        private var showSpecialApps = false
+        private var showAllApps: Boolean? = null
+
+        fun setOnAppAddListener(onAppAddListener: OnAppAddListener): Builder {
+            this.onAppAddListener = onAppAddListener
+            return this
+        }
+
+        fun activeApps(activeApps: Set<String>): Builder {
+            this.activeApps = activeApps
+            return this
+        }
+
+        fun showSpecialApps(show: Boolean): Builder {
+            this.showSpecialApps = show
+            return this
+        }
+
+        fun showAllApps(show: Boolean?): Builder {
+            this.showAllApps = show
+            return this
+        }
+
+        fun build(): InstalledApplicationsManager =
+            InstalledApplicationsManager(
+                onAppAddListener,
+                activeApps,
+                showSpecialApps,
+                showAllApps
+            )
     }
 }
