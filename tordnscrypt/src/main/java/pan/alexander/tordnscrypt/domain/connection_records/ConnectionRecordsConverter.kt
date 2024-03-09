8443a13b5e734a0772960872416c109003836c42
Oleksandr Garmatin <invizible.soft@gmail.com>
2024-03-09 11:25:45 +0200

    fix: show blocked forward DNS queries

--- tordnscrypt/src/main/java/pan/alexander/tordnscrypt/domain/connection_records/ConnectionRecordsConverter.kt
+++ tordnscrypt/src/main/java/pan/alexander/tordnscrypt/domain/connection_records/ConnectionRecordsConverter.kt
@@ -337,7 +337,7 @@ class ConnectionRecordsConverter @Inject constructor(
 
         if (consumed) {
             logRecords.add(packetLogEntry)
-        } else if (vpnDNS == null || !vpnDNS.contains(packetRecord.daddr)) {
+        } else if (vpnDNS == null || !vpnDNS.contains(packetRecord.daddr) || !packetRecord.allowed) {
             if (!meteredNetwork && packetRecord.daddr.isNotEmpty()) {
                 val host = ipToHostAddressMap[IpToTime(packetRecord.daddr)]
                 if (host == null) {
