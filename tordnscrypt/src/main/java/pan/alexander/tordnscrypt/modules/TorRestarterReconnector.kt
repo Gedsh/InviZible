38f53a5d4299e1d0f533a49362fcc2607a040a7f
Oleksandr Garmatin <invizible.soft@gmail.com>
2025-07-21 10:30:16 +0300

    fix: do not delete the status file to prevent guard rotation

--- tordnscrypt/src/main/java/pan/alexander/tordnscrypt/modules/TorRestarterReconnector.kt
+++ tordnscrypt/src/main/java/pan/alexander/tordnscrypt/modules/TorRestarterReconnector.kt
@@ -133,7 +133,6 @@ class TorRestarterReconnector @Inject constructor(
         FileManager.deleteFileSynchronous(context, pathVars.appDataDir + "/tor_data", "cached-microdesc-consensus")
         //FileManager.deleteFileSynchronous(context, pathVars.appDataDir + "/tor_data", "cached-microdescs")
         //FileManager.deleteFileSynchronous(context, pathVars.appDataDir + "/tor_data", "cached-microdescs.new")
-        FileManager.deleteFileSynchronous(context, pathVars.appDataDir + "/tor_data", "state")
     }
 
     fun stopRestarterCounters() {
